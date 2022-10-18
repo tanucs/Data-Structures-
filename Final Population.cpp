9fd3d396910fac78fa815c10a8c51d808d5ff85f
Tanu Upadhyay <96697977+tanucs@users.noreply.github.com>
2022-10-18 16:30:04 +0530

    Create Final Population.cpp

---
+++ Final Population.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    
+    while(T--){
+        int X,Y,Z,A;
+        cin>>X>>Y>>Z;
+        A=(X-Y)+Z;
+        cout<<A<<endl;
+    }
+
+	return 0;
+}
